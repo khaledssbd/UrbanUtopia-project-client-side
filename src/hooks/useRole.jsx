13bd15a7f2361702d6716cea267470510ed327e4
khaledshuvo <khaledssbd@hotmail.com>
2024-06-09 03:31:06 +0600

    useRole done

--- src/hooks/useRole.jsx
+++ src/hooks/useRole.jsx
@@ -1,8 +1,23 @@
-
+import useAuth from './useAuth';
+import useAxiosSecure from './useAxiosSecure';
+import { useQuery } from '@tanstack/react-query';
 
 const useRole = () => {
-  
+  const { user, loading } = useAuth();
+  const axiosSecure = useAxiosSecure();
+  const token = localStorage.getItem('access-token');
+
+  const { data: role = '', isLoading: isRoleLoading } = useQuery({
+    queryKey: ['role', user?.email],
+    enabled: !loading && !!user?.email && !!token,
+    queryFn: async () => {
+      const { data } = await axiosSecure.get(
+        `/user/role/${user?.email}?email=${user?.email}`
+      );
+      return data;
+    },
+  });
+  return [role, isRoleLoading];
 };
 
 export default useRole;
-
